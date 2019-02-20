c3a339d99b08e1e19c6580dad8297afd6f7b5003
nsheridan <nik.sheridan@gmail.com>
2019-02-20 17:34:46 +0000

    modifying bats execution path

--- bats/bats_test.sh
+++ bats/bats_test.sh
@@ -4,3 +4,14 @@
 	run stat /tmp
 	[ $status = 0 ]
 }
+
+@test "Check that total is listed" {
+	run ls -l
+	[[ ${lines[0]} =~ "total" ]]
+}
+
+@test "A test I don't want to execute for now" {
+	skip "feature coming"
+	run foo
+	[ $status -eq 0 ]
+}
